1a9f882d4edf79ad65b705062be975100a15d1ca
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-06-28 23:38:03 +0600

    add responsive footer and navbar

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -19,11 +19,11 @@ const NavBar = () => {
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                     </div>
                     <ul
                         tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        className="menu menu-sm dropdown-content bg-black rounded-box z-1 mt-3 w-52 p-2 shadow">
                         {
                             navLinks
                         }
@@ -31,7 +31,7 @@ const NavBar = () => {
                 </div>
 
                 <Link to="/">
-                    <img className='w-28' src={logo} alt="" />
+                    <img className='md:w-28 w-16' src={logo} alt="" />
                 </Link>
 
             </div>
@@ -46,7 +46,7 @@ const NavBar = () => {
                 <a
                     target='_blank'
                     href='https://drive.google.com/file/d/1xozYHydK6FAVCWWyRFwK7ie0O0Vu9i37/view?usp=drive_link'
-                    className="bg-gray-700 text-white text-lg font-bold border-0 rounded-xl px-6 py-2 cursor-pointer hover:bg-white hover:text-black">
+                    className="bg-gray-700 text-white md:text-lg font-bold border-0 rounded-xl md:px-6 px-3 py-2 cursor-pointer hover:bg-white hover:text-black">
                     My Resume
                 </a>
             </div>
