021a04a3a13b2ae8e3750898341a800eb505be3e
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-06-29 00:11:37 +0600

    add banner section

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import profile from "../../assets/Ashik.profile_1.0.jpg"
+
+const Banner = () => {
+    return (
+        <div>
+            <div className='flex justify-center items-center gap-6 bg-gradient-to-br from-[#2b2b2b] to-[#131212] p-12 rounded-3xl'>
+                <div>
+                    <img className='rounded-tl-3xl rounded-br-3xl w-72' src={profile} alt="" />
+                </div>
+                <div>
+                    <p className='text-gray-400 text-lg font-bold'>
+                        APPLICATION DEVELOPER
+                    </p>
+                    <h1 className='text-4xl font-bold'>
+                        Syed Ashik Mahmud Pullock
+                    </h1>
+                    <p className='text-gray-400'>
+                        React, MERN Stack Developer & Learner
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
