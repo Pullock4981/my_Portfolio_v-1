c90dbe241af9a172748ae4157c8840941866f5eb
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2025-06-28 23:20:57 +0600

    add footer and navbar

--- src/Shared/Footer.jsx
+++ src/Shared/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import logo from '../assets/logo.png'
 
 const Footer = () => {
     return (
-        <div>
-            <h1>
-                this is footer
-            </h1>
-        </div>
+        <footer className="footer footer-horizontal footer-center bg-black text-primary-content p-10">
+            <aside>
+                <img
+                    className='h-20'
+                    src={logo} alt="" />
+                
+                <p>© All rights reserved by <span className='text-[#5B78F6]'>Syed Ashik Mahmud Pullock</span></p>
+            </aside>
+        </footer>
     );
 };
 
--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import logo from "../assets/logo.png"
 import { Link, NavLink } from 'react-router';
+import './Navbar.css';
 
 const NavBar = () => {
     // navLinks
     const navLinks = <>
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/skills">Skills</NavLink></li>
-        <li><NavLink to="/projects">Projects</NavLink></li>
-        <li><NavLink to="/contact">Contact</NavLink></li>
+        <div className='flex md:flex-row flex-col gap-4 text-lg font-bold'>
+            <li className='text-gray-400 hover:text-white'><NavLink to="/">Home</NavLink></li>
+            <li className='text-gray-400 hover:text-white'><NavLink to="/about">About</NavLink></li>
+            <li className='text-gray-400 hover:text-white'><NavLink to="/skills">Skills</NavLink></li>
+            <li className='text-gray-400 hover:text-white'><NavLink to="/projects">Projects</NavLink></li>
+            <li className='text-gray-400 hover:text-white'><NavLink to="/contact">Contact</NavLink></li>
+        </div>
     </>
     return (
-        <div className="navbar bg-black text-white max-w-7xl mx-auto shadow-sm">
+        <div className="navbar bg-black text-white max-w-7xl mx-auto md:px-20 py-4 shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -27,7 +30,7 @@ const NavBar = () => {
                     </ul>
                 </div>
 
-                <Link>
+                <Link to="/">
                     <img className='w-28' src={logo} alt="" />
                 </Link>
 
@@ -40,7 +43,12 @@ const NavBar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Button</a>
+                <a
+                    target='_blank'
+                    href='https://drive.google.com/file/d/1xozYHydK6FAVCWWyRFwK7ie0O0Vu9i37/view?usp=drive_link'
+                    className="bg-gray-700 text-white text-lg font-bold border-0 rounded-xl px-6 py-2 cursor-pointer hover:bg-white hover:text-black">
+                    My Resume
+                </a>
             </div>
         </div>
     );
